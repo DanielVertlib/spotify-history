b860eadb05c2c04aa0eca1019ad4b45a0b8b33a5
DanielVertlib <danielvertlib@hotmail.ca>
2020-03-26 21:35:07 -0400

    Add authorization for spotify api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+export const authEndpoint = 'https://accounts.spotify.com/authorize'
+
+// App's client ID, redirect URI and desired scopes
+const clientId = 'f6fb11fc92c64dc491d0bd2b5473ae12'
+const redirectUri = 'http://localhost:3000'
+
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      token: null
+    }
+  }
+
+  componentDidMount() {
+    // Set token
+    const hash = window.location.hash
+    if(hash) {
+      console.log(hash)
+      const params = {}
+      window.location.hash
+        .substring(1)
+        .split('&')
+        .forEach(param => {
+          const item = param.split('=')
+          params[item[0]] = decodeURIComponent(item[1])
+        })
+
+      this.setState({ token: params })
+    }
+  }
+
+  render() {
+    console.log(this.state)
+    return (
+      <div className="App">
+        <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        {!this.state.token && (
+          <a
+            className="btn btn--loginApp-link"
+            href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=token&show_dialog=true`}
+          >
+            Login to Spotify
+          </a>
+        )}
+        </header>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
